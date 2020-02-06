727a5baac634bf24da2c2601f7c472ab5938e3eb
fldmxp <305624874@qq.com>
2020-02-06 10:50:13 +0800

    test6

---
+++ hello - 副本 (2).go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println(25)
+}
+
+//main方法
+func main() {
+	fmt.Println(isPrime(25))
+	fmt.Println(isPrime(25))
+}
+
+func isPrime(value int) bool {
+	if value <= 3 {
+		return value >= 2
+	}
+	if value%2 == 0 || value%3 == 0 {
+		return false
+	}
+	for i := 5; i*i <= value; i += 6 {
+		if value%i == 0 || value%(i+2) == 0 {
+			return false
+		}
+	}
+	return true
+}
